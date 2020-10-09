dd5c039cb060e6c5fc9875265ecc7689364f0fbb
Hemal Buha <buhahemal10@gmail.com>
2020-10-10 00:30:51 +0530

    Userprofile Error resolve

--- src/UserProfile.php
+++ src/UserProfile.php
@@ -6,6 +6,6 @@ class UserProfile
     public function Usermeta()
     {
         $User = new Edminglecurl('/user/usermeta','GET','1');
-        return json_decode($User->Get_curl_no_auth());
+        return json_decode($User->Get_curl_auth());
     }
 }
