6535f1970e8dbfd1288ae42421bbf81ef824357c
Hemal Buha <buhahemal10@gmail.com>
2020-10-21 15:36:27 +0530

    Changes Made on 21/10

--- src/Courses.php
+++ src/Courses.php
@@ -8,6 +8,22 @@ class Courses
       $Allcourse = new Edminglecurl('/institute/1/courses','GET','0');
       return json_decode($Allcourse->Get_curl_no_auth());
     }
+
+    public function CourseDetail($course_id)
+    {
+      $course_detail = new Edminglecurl('/bundles/'.$course_id.'','GET','0');
+      return json_decode($course_detail->Get_curl_no_auth());
+    }
+
+    public function CourseSchedule($course_id)
+    {
+      $course_schedule = new Edminglecurl('/public/bundle/'.$course_id.'/schedule','GET','0');
+      return json_decode($course_schedule->Get_curl_no_auth());
+    }
+
+
 }
 $c = new Courses();
-print_r($c->GetAllCourse());
+//print_r($c->GetAllCourse());
+//print_r($c->CourseDetail(20));
+//print_r($c->CourseSchedule(10));
---
+++ src/Tutor.php
@@ -0,0 +1,13 @@
+<?php 
+namespace edmingle_php_sdk;
+require('Edminglecurl.php');
+class Tutor 
+{
+    public function Tutor_info($id)
+    {
+      $Tutor_info = new Edminglecurl('/tutor/'.$id.'/profile/public','GET','0');
+      return json_decode($Tutor_info->Get_curl_no_auth());
+    }
+}
+//$t = new Tutor();
+//print_r($t->Tutor_info(1));
